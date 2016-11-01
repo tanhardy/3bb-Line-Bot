ea5694a7bc8165e4401fec087d0109531940e41b
qostttbb <qostttbb@gmail.com>
2016-11-01 11:43:24 +0700

    Update 3bb-line-bot.php

--- 3bb-line-bot.php
+++ 3bb-line-bot.php
@@ -22,14 +22,14 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $userId,	//$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$userId],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
