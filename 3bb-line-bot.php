437d790995f1b450a9fc742156071bb18ece62b3
qostttbb <qostttbb@gmail.com>
2016-11-01 10:22:45 +0700

    Update 3bb-line-bot.php

--- 3bb-line-bot.php
+++ 3bb-line-bot.php
@@ -1,4 +1,6 @@
 <?php
+$proxy = 'http://fixie:Flxod6VSpeItsgI@velodrome.usefixie.com:80';
+$proxyauth = 'qostttbb@gmail.com:noqnoq123';
 $access_token = 'bjMobsS6ndui8SI7L3yfrJTH/J70rI8N/UdD2xX6vKOJQgbooymVfPIiDMOer8HbfO4rhEtXypseVhC9nY4HoRIUnZQ3j0CGVFwLc8Pq/a4lddesAE/PjK06ihIXeq+rw/gWf+5jD/Lf8ToVJPvNuwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -37,6 +39,8 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
