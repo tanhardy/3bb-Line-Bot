03f040c7c2237f3e3ee5a1b9caf2ae953454df20
tanhardy <39728548+tanhardy@users.noreply.github.com>
2018-05-30 14:48:08 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,11 +1,9 @@
 <?php
-$proxy = 'http://fixie:Flxod6VSpeItsgI@velodrome.usefixie.com:80';
-$proxyauth = 'qostttbb@gmail.com:noqnoq123';
 $access_token = 'bjMobsS6ndui8SI7L3yfrJTH/J70rI8N/UdD2xX6vKOJQgbooymVfPIiDMOer8HbfO4rhEtXypseVhC9nY4HoRIUnZQ3j0CGVFwLc8Pq/a4lddesAE/PjK06ihIXeq+rw/gWf+5jD/Lf8ToVJPvNuwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('g9dQqU7Lew8MZMX8Mj1daewAER0g4eygxB/FLDUbMecNMAJ7vyBDOtyHk2osLE1540sMUNVyDoMB1QN+708xZOyfVsFjHH3H5dqG87PenA/zLKndQskuG8WuEXYrLiHUyHmiaZVd7sbgkSAgK73O7AdB04t89/1O/w1cDnyilFU= ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
